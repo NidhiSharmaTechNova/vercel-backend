564cd0c6d1c39e7b18cfb320e93bf919ecfeef4a
Saloni Sharma <saloni94666@gmail.com>
2026-04-05 13:58:07 +0530

    fix token error

--- middleware/userAuth.js
+++ middleware/userAuth.js
@@ -26,30 +26,51 @@
 
 // export default userAuth;
 
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-const userAuth = async (req, res, next) => {
-   try {
-       const authHeader = req.headers.authorization;
+// const userAuth = async (req, res, next) => {
+//    try {
+//        const authHeader = req.headers.authorization;
 
-       if (!authHeader) {
-           return res.json({ success: false, message: "No token provided" });
-       }
+//        if (!authHeader) {
+//            return res.json({ success: false, message: "No token provided" });
+//        }
 
-       const token = authHeader.split(" ")[1];
+//        const token = authHeader.split(" ")[1];
 
-       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//        const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-       req.userId = decoded.id;
+//        req.userId = decoded.id;
 
-       next();
+//        next();
 
-   } catch (error) {
-       return res.json({ success: false, message: error.message });
-   }
-}
+//    } catch (error) {
+//        return res.json({ success: false, message: error.message });
+//    }
+// }
 
-export default userAuth;
+// export default userAuth;
+
+import jwt from "jsonwebtoken";
+
+export const userAuth = (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      return res.json({ success: false, message: "No token provided" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.userId = decoded.id;
+
+    next();
+
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
 
 // import jwt from 'jsonwebtoken';
 
