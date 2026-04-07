0860ad276f3f65ca34a38a672411638e102f0220
Saloni Sharma <saloni94666@gmail.com>
2026-04-07 14:43:48 +0530

    add cors

--- server.js
+++ server.js
@@ -16,26 +16,26 @@ connectdb();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-  origin: true,
-  credentials: true
-}));
-
-// const allowedOrigins = [
-//   "http://localhost:5173",
-//   "https://vercel-frontend-three-olive.vercel.app"
-// ];
-
 // app.use(cors({
-//   origin: function (origin, callback) {
-//     if (!origin || allowedOrigins.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed by CORS"));
-//     }
-//   },
+//   origin: true,
 //   credentials: true
 // }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://vercel-frontend-three-olive.vercel.app"
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+}));
 
 app.get('/',(req, res)=>{
    res.send('API working fine')
